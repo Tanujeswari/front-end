2e8faa36e2209734c3d66b33c044ca1067760906
Tanujeswari <125129809+Tanujeswari@users.noreply.github.com>
2025-03-12 12:07:57 +0530

    Front_end

---
+++ person.js
@@ -0,0 +1,12 @@
+const person = { 
+    firstName: "rahul",
+    lastName: "koppula",
+};
+
+// first way: using dot notation
+console.log(person.firstName); // rahul
+console.log(person.lastName);  // koppula
+
+// second way: using bracket notation
+console.log(person["firstName"]); // rahul
+console.log(person["lastName"]);  // koppula
